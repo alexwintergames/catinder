23f620cd15a33b8e25fd6373300a240eab8a0e91
ALEXSANDER FIDELIS MOREIRA <alexsander.moreira@alunos.sc.senac.br>
2025-10-23 08:33:33 -0300

    Enviando projeto

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Image, Button, StyleSheet } from "react-native";
 
 export default function App() {
+  const animais = [
+    require("./assets/gato1.jpg"),
+    require("./assets/gato2.png"),
+    require("./assets/gato3.jpg"),
+    require("./assets/gato4.jpg"),
+    require("./assets/gato5.avif"),
+    require("./assets/gato6.webp"),
+    require("./assets/gato7.webp"),
+  ];
+
+  const [indice, setIndice] = useState(0);
+  const [corBorda, setCorBorda] = useState("#000");
+
+  function trocarImagem() {
+    const novoIndice = (indice + 1) % animais.length;
+    setIndice(novoIndice);
+  }
+
+  function gostei() {
+    setCorBorda("green");
+    setTimeout(() => {
+      setCorBorda("#000");
+      trocarImagem();
+    }, 1000);
+  }
+
+  function naoGostei() {
+    setCorBorda("red");
+    setTimeout(() => {
+      setCorBorda("#000");
+      trocarImagem();
+    }, 1000);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={animais[indice]}
+        style={[styles.imagem, { borderColor: corBorda }]}
+      />
+      <View style={styles.botoes}>
+        <Button title="Gostei" color="green" onPress={gostei} />
+        <Button title="Não Gostei" color="red" onPress={naoGostei} />
+      </View>
     </View>
   );
 }
@@ -13,8 +53,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  imagem: {
+    width: 300,
+    height: 300,
+    borderWidth: 5,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  botoes: {
+    flexDirection: "row",
+    gap: 10,
   },
 });
